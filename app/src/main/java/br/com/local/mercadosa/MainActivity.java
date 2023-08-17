d87f588d82ec2706ab0cca85c9b6bd6b2fe9a62b
lndsilva <lndsilva@hotmail.com>
2023-08-17 14:57:06 -0300

    criando objetos para lista

--- app/src/main/java/br/com/local/mercadosa/MainActivity.java
+++ app/src/main/java/br/com/local/mercadosa/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,13 +14,115 @@ public class MainActivity extends AppCompatActivity {
     List<Mercadoria> listaMercadorias;
     RecyclerView idRecyclerView;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         idRecyclerView = findViewById(R.id.idRecylerView);
+
+        listaMercadorias = new ArrayList<>();
+
+        listaMercadorias.add(
+                new Mercadoria(
+                        "Orfeu, Café em Grãos Clássico, 1kg",
+                        10,
+                        69.90,
+                        "9",
+                        R.drawable.cafe
+                )
+
+        );
+        listaMercadorias.add(
+                new Mercadoria(
+                        "Mostarda Heinz 255g",
+                        5,
+                        10.79,
+                        "8",
+                        R.drawable.mostarda
+                )
+
+        );
+        listaMercadorias.add(
+                new Mercadoria(
+                        "Balança Digital",
+                        10,
+                        69.90,
+                        "9",
+                        R.drawable.balanca
+                )
+
+        );
+        listaMercadorias.add(
+                new Mercadoria(
+                        "Echo Dot 5ª geração",
+                        10,
+                        69.90,
+                        "9",
+                        R.drawable.echo
+                )
+
+        );
+        listaMercadorias.add(
+                new Mercadoria(
+                        "Fritadeira Pratic Fryer",
+                        10,
+                        69.90,
+                        "9",
+                        R.drawable.fritadeira
+                )
+
+        );
+        listaMercadorias.add(
+                new Mercadoria(
+                        "Aptanutri Profutura 3",
+                        10,
+                        69.90,
+                        "9",
+                        R.drawable.leite
+                )
+
+        );
+        listaMercadorias.add(
+                new Mercadoria(
+                        "Oster Liquidificador",
+                        10,
+                        69.90,
+                        "9",
+                        R.drawable.liquidificador
+                )
+
+        );
+        listaMercadorias.add(
+                new Mercadoria(
+                        "Mochila de viagem grande",
+                        10,
+                        69.90,
+                        "9",
+                        R.drawable.mochila
+                )
+
+        );
+        listaMercadorias.add(
+                new Mercadoria(
+                        "Vancouver Panela de Pressão",
+                        10,
+                        69.90,
+                        "9",
+                        R.drawable.panela
+                )
+
+        );
+        listaMercadorias.add(
+                new Mercadoria(
+                        "Tênis Olympikus",
+                        10,
+                        69.90,
+                        "9",
+                        R.drawable.tenis
+                )
+
+        );
 
 
         MyAdapter adapter = new MyAdapter(getApplicationContext(), listaMercadorias);
